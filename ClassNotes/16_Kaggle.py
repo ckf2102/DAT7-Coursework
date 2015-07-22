5db79786c615e0c6705fc445062459c1665c4dd6
Corinne Fukayama <corinne.fukayama@gmail.com>
2015-07-22 16:20:03 -0400

    kaggle comp and hw15

---
+++ ClassNotes/16_Kaggle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 22 12:02:21 2015
+
+@author: Corinne
+"""
+
+import pandas as pd
+test = pd.read_csv('test.csv')
+train = pd.read_csv('train.csv')
+
+test.head()
